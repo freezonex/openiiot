4e99d64b59a05d8f5c9fcca0769849ea5739a07a
hongzhi1234 <zhanghongzhi@freezonex.io>
2024-02-19 14:06:45 +0800

    modify openiiot service account

--- deployment/k8s/openiiot_install.sh
+++ deployment/k8s/openiiot_install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Check if the namespace "openiiot" does not exist
+if ! kubectl get namespace openiiot > /dev/null 2>&1; then
+    echo "Namespace 'openiiot' does not exist. Please use "kubectl create namespace openiiot" to create it before running this script."
+    exit 1
+fi
+
 # docker load -i all images
 kubectl create -f create-service-account.yaml
 kubectl create -f role-binding.yaml
