e56f668fde3546253483a2d669de88223466ef40
hongzhi1234 <zhanghongzhi@freezonex.io>
2024-02-28 13:58:20 +0800

    modify role

--- deployment/k8s/openiiot_install.sh
+++ deployment/k8s/openiiot_install.sh
@@ -9,4 +9,18 @@ fi
 # docker load -i all images
 kubectl create -f create-service-account.yaml
 kubectl create -f role-binding.yaml
+
+# Set the ServiceAccount name and namespace
+SERVICE_ACCOUNT_NAME="admin-user"
+NAMESPACE="kube-system"
+
+# when k8s >= v1.24.0, secret will not be automatically generated once ServiceAccount created, needs create manually
+# Get the Secret name associated with the ServiceAccount
+SECRET_NAME=$(kubectl get serviceaccount $SERVICE_ACCOUNT_NAME -n $NAMESPACE -o jsonpath="{.secrets[0].name}")
+
+# Get and print Token
+echo "Token for ServiceAccount $SERVICE_ACCOUNT_NAME in $NAMESPACE namespace:"
+kubectl get secret $SECRET_NAME -n $NAMESPACE -o jsonpath="{.data.token}" | base64 --decode
+echo
+
 kubectl describe secret $secret_name -n kube-system | grep token
