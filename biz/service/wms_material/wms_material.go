4c5e8fb9943603e40d5ebf994b88f66bae8420db
Wenhao <yuwenhao@freezonex.io>
2024-03-04 18:26:26 +0800

    wms update

--- biz/service/wms_material/wms_material.go
+++ biz/service/wms_material/wms_material.go
@@ -5,6 +5,7 @@ import (
 	"freezonex/openiiot/biz/middleware"
 	"freezonex/openiiot/biz/model/freezonex_openiiot_api"
 	"freezonex/openiiot/biz/service/utils/common"
+	storagelocation "freezonex/openiiot/biz/service/wms_storage_location"
 	"github.com/cloudwego/hertz/pkg/app"
 	logs "github.com/cloudwego/hertz/pkg/common/hlog"
 )
@@ -35,16 +36,19 @@ func (a *WmsMaterialService) GetWmsMaterial(ctx context.Context, req *freezonex_
 	resp := new(freezonex_openiiot_api.GetMaterialResponse)
 	data := make([]*freezonex_openiiot_api.Material, 0)
 	for _, v := range wmss {
+		storageLocationService := storagelocation.DefaultStorageLocationService()
+		storageLocationData, _ := storageLocationService.GetStorageLocationDB(ctx, *v.StorageLocationID, "", nil, "")
+
 		data = append(data, &freezonex_openiiot_api.Material{
-			Id:          common.Int64ToString(v.ID), // Converts int64 ID to string using a custom common package function
-			ProductCode: v.ProductCode,
-			Name:        v.Name,
-			//StorageLocation:             v.StorageLocationID,
-			ProductType: *v.ProductType,
-			Unit:        *v.Unit,
-			Note:        *v.Note,
-			CreateTime:  common.GetTimeStringFromTime(&v.CreateTime), // Converts time.Time to string using a custom common package function
-			UpdateTime:  common.GetTimeStringFromTime(&v.UpdateTime), // Converts time.Time to string using a custom common package function
+			Id:              common.Int64ToString(v.ID), // Converts int64 ID to string using a custom common package function
+			ProductCode:     v.ProductCode,
+			Name:            v.Name,
+			StorageLocation: storageLocationData[0].Name,
+			ProductType:     *v.ProductType,
+			Unit:            *v.Unit,
+			Note:            *v.Note,
+			CreateTime:      common.GetTimeStringFromTime(&v.CreateTime), // Converts time.Time to string using a custom common package function
+			UpdateTime:      common.GetTimeStringFromTime(&v.UpdateTime), // Converts time.Time to string using a custom common package function
 		})
 	}
 	resp.Data = data
