5c1b776dd302403784a939fc5ef0bdef4837be13
Wenhao <yuwenhao@freezonex.io>
2024-03-09 21:34:47 +0800

    wms update

--- biz/service/wms_storagelocationmaterial/common.go
+++ biz/service/wms_storagelocationmaterial/common.go
@@ -1,8 +1,9 @@
 package wms_storagelocationmaterial
 
 import (
-	"freezonex/openiiot/biz/dal/mysql"
 	"sync"
+
+	"freezonex/openiiot/biz/dal/mysql"
 )
 
 type WmsStorageLocationMaterialService struct {
