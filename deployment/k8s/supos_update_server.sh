4f5519099236de950d960510987acabc1400f676
hongzhi1234 <144645456+hongzhi1234@users.noreply.github.com>
2024-03-12 02:59:53 +0800

    Update supos_update_server.sh

--- deployment/k8s/supos_update_server.sh
+++ deployment/k8s/supos_update_server.sh
@@ -6,3 +6,4 @@ echo "stop old deployment successfully"
 docker load -i deployment/binary/openiiot_server.tar
 kubectl apply -f deployment/k8s/openiiot-server-deployment.yaml
 echo "start new server deployment successfully"
+
