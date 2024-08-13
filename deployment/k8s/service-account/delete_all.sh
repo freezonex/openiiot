69647349e18358ec49adffafa5884fa6e6991043
hongzhi1234 <zhanghongzhi@freezonex.io>
2024-08-13 17:42:12 +0800

    update Dockerfile, remove unnecessary files

--- deployment/k8s/service-account/delete_all.sh
+++
@@ -1,9 +0,0 @@
-# check serviceaccount status
-kubectl describe serviceaccount admin-user -n kube-system
-kubectl describe secret admin-user-manual-token -n kube-system
-kubectl describe secret admin-user-manual-token -n kube-system
-
-# delete serviceaccount
-kubectl delete clusterrolebinding admin-user -n kube-system
-kubectl delete secret admin-user-manual-token -n kube-system
-kubectl delete serviceaccount admin-user -n kube-system
